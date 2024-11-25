27fa47312829753b31ea257b9b9bc9f7b5854ae9
sk1ldpadde <artisan-active0w@icloud.com>
2024-11-25 14:44:18 +0100

    fixed errors

--- DAG_Create_tables.py
+++ DAG_Create_tables.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
@@ -12,46 +11,45 @@ from airflow.operators.hdfs_operations import HdfsPutFileOperator, HdfsGetFileOp
 from airflow.operators.filesystem_operations import CreateDirectoryOperator
 from airflow.operators.filesystem_operations import ClearDirectoryOperator
 from airflow.operators.hive_operator import HiveOperator
+from airflow.operators.python_operator import PythonOperator
 
 args = {
     'owner': 'airflow'
 }
 
-
 hiveSQL_create_bike_share_trips_table = '''
-CREATE EXTERNAL TABLE IF NOT EXISTS bike_share_trips (
-    tripduration INT,
-    starttime TIMESTAMP,
-    stoptime TIMESTAMP,
-    `start station id` INT,
-    `start station name` STRING,
-    `start station latitude` DOUBLE,
-    `start station longitude` DOUBLE,
-    `end station id` INT,
-    `end station name` STRING,
-    `end station latitude` DOUBLE,
-    `end station longitude` DOUBLE,
-    bikeid INT,
-    usertype STRING,
-    `birth year` INT,
-    gender INT
-)
-COMMENT 'Bike data' 
-PARTITIONED BY (partition_year INT, partition_month INT) 
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' -- Assuming CSV file
-STORED AS TEXTFILE LOCATION '/user/hadoop/bike_share/bike_share_trips'
-TBLPROPERTIES ('skip.header.line.count'='1');
-'''
+    CREATE EXTERNAL TABLE IF NOT EXISTS bike_share_trips (
+        tripduration INT,
+        starttime TIMESTAMP,
+        stoptime TIMESTAMP,
+        `start station id` INT,
+        `start station name` STRING,
+        `start station latitude` DOUBLE,
+        `start station longitude` DOUBLE,
+        `end station id` INT,
+        `end station name` STRING,
+        `end station latitude` DOUBLE,
+        `end station longitude` DOUBLE,
+        bikeid INT,
+        usertype STRING,
+        `birth year` INT,
+        gender INT
+    )
+    COMMENT 'Bike data'
+    PARTITIONED BY (partition_year INT, partition_month INT)
+    ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' -- Assuming CSV file
+    STORED AS TEXTFILE LOCATION '/user/hadoop/bike_share/bike_share_trips'
+    TBLPROPERTIES ('skip.header.line.count'='1');
+    '''
 
 hiveSQL_add_partition_bike_share_trips = '''
-ALTER TABLE bike_share_trips ADD IF NOT EXISTS partition(partition_year={{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}, partition_month={{ macros.ds_format(ds, "%Y-%m-%d", "%m")}})
-LOCATION '/user/hadoop/bike_share/bike_share_trips/{{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}/{{ macros.ds_format(ds, "%Y-%m-%d", "%m")}}/'; 
+    ALTER TABLE bike_share_trips ADD IF NOT EXISTS partition(partition_year={{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}, partition_month={{ macros.ds_format(ds, "%Y-%m-%d", "%m")}})
+    LOCATION '/user/hadoop/bike_share/bike_share_trips/{{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}/{{ macros.ds_format(ds, "%Y-%m-%d", "%m")}}/';
 
-LOAD DATA INPATH '/user/hadoop/bike_share/bike_share_trips/2024/11/bike_share_{{ ds }}.csv' INTO TABLE bike_share_trips;
-'''
+    LOAD DATA INPATH '/user/hadoop/bike_share/bike_share_trips/2024/11/bike_share_{{ ds }}.csv' INTO TABLE bike_share_trips;
+    '''
 
-
-dag = DAG('DAG for hubway', default_args=args, description='KPI Pipeline',
+dag = DAG('DAGforhubway', default_args=args, description='KPI Pipeline',
           schedule_interval='56 18 * * *',
           start_date=datetime(2024, 11, 13), catchup=False, max_active_runs=1)
 
@@ -75,18 +73,32 @@ clear_local_import_directory = ClearDirectoryOperator(
     dag=dag,
 )
 
-download_bike_share_data = HttpDownloadOperator(
+
+def download_data(ds, **kwargs):
+    import os
+    from kaggle.api.kaggle_api_extended import KaggleApi
+
+    # Authenticate using Kaggle API credentials
+    api = KaggleApi()
+    api.authenticate()
+
+    # Define the path where data will be downloaded
+    download_path = '/home/airflow/bike_share/csv'
+    if not os.path.exists(os.path.dirname(download_path)):
+        os.makedirs(os.path.dirname(download_path))
+
+    # Download the dataset
+    api.dataset_download_files(
+        'acmeyer/hubway-data', path=download_path, unzip=True)
+
+
+download_bike_share_data = PythonOperator(
     task_id='download_bike_share_dataset',
-    download_uri='https://www.kaggle.com/api/v1/datasets/download/acmeyer/bike-share-data',
-    save_to='/home/airflow/bike_share/bike_share_{{ ds }}.zip',
+    python_callable=download_data,
+    op_kwargs={'ds': '{{ ds }}'},
     dag=dag,
 )
 
-unzip_bike_share_data = BashOperator(
-    task_id='unzip_bike_share_dataset',
-    bash_command='unzip /home/airflow/bike_share/bike_share_{{ ds }}.zip -d /home/airflow/bike_share/',
-    dag=dag,
-)
 
 # Create CSV directory for bike share data
 create_bike_share_csv_directory = CreateDirectoryOperator(
@@ -104,7 +116,7 @@ filter_and_copy_bike_share_csv = BashOperator(
 
 merge_bike_share_csv_files = BashOperator(
     task_id='merge_bike_share_csv_files',
-    bash_command='cd /home/airflow/bike_share/csv && csvstack *.csv > bike_share_{{ ds }}.csv',
+    bash_command='cd /home/airflow/bike_share/csv&& rename "s/[ ^ a-zA-Z0-9_.-]/_/g" *.csv && csvstack *.csv > bike_share_{{ ds }}.csv',
     dag=dag,
 )
 
@@ -116,7 +128,6 @@ create_hdfs_bike_share_partition_directory = HdfsMkdirFileOperator(
     dag=dag,
 )
 
-
 hdfs_upload_bike_share_data = HdfsPutFileOperator(
     task_id='upload_bike_share_data_to_hdfs',
     local_file='/home/airflow/bike_share/csv/bike_share_{{ ds }}.csv',
@@ -124,7 +135,6 @@ hdfs_upload_bike_share_data = HdfsPutFileOperator(
     hdfs_conn_id='hdfs',
     dag=dag,
 )
-
 
 create_HiveTable_bike_share_trips = HiveOperator(
     task_id='create_bike_share_trips_table',
@@ -150,9 +160,16 @@ kpi_gen = SparkSubmitOperator(
     num_executors='2',
     name='spark_gen_kpi',
     verbose=True,
-    application_args=['--year', '{{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}', '--month', '{{ macros.ds_format(ds, "%Y-%m-%d", "%m")}}', '--day',  '{{ macros.ds_format(ds, "%Y-%m-%d", "%d")}}', '--hdfs_source_dir', '/user/hadoop/hubway'],
+    conf={
+        'spark.driver.userClassPathFirst': 'true',
+        'spark.executor.userClassPathFirst': 'true',
+        'spark.driver.extraClassPath': '/home/hadoop/hadoop/share/hadoop/common/lib/slf4j-log4j12-1.7.25.jar',
+        'spark.executor.extraClassPath': '/home/hadoop/hadoop/share/hadoop/common/lib/slf4j-log4j12-1.7.25.jar',
+    },
+    application_args=['--year', '{{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}', '--month', '{{ macros.ds_format(ds, "%Y-%m-%d", "%m")}}',
+                      '--day',  '{{ macros.ds_format(ds, "%Y-%m-%d", "%d")}}', '--hdfs_input_path', '/user/hadoop/hubway'],
     dag=dag
 )
 
-
-create_local_import_directory >> remove_old_csv >> clear_local_import_directory >> download_bike_share_data >> unzip_bike_share_data >> create_bike_share_csv_directory >> filter_and_copy_bike_share_csv >> merge_bike_share_csv_files >> create_hdfs_bike_share_partition_directory >> hdfs_upload_bike_share_data >> kpi_gen
+create_local_import_directory >> remove_old_csv >> clear_local_import_directory >> download_bike_share_data >> create_bike_share_csv_directory >> filter_and_copy_bike_share_csv >> merge_bike_share_csv_files >> create_hdfs_bike_share_partition_directory >> hdfs_upload_bike_share_data >> kpi_gen
+create_HiveTable_bike_share_trips >> addPartition_HiveTable_bike_share_trips
--- kpi_generation.py
+++ kpi_generation.py
@@ -12,68 +12,86 @@ import openpyxl
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Spark Job for Bike Share Data Analysis')
-    parser.add_argument('--hdfs_input_path', help='HDFS input directory', required=True, type=str)
-    parser.add_argument('--year', help='Partion Year To Process, e.g. 2024', required=True, type=str)
-    parser.add_argument('--month', help='Partion Month To Process, e.g. 11', required=True, type=str)
-    parser.add_argument('--day', help='Day of File To Process, e.g. 23', required=True, type=str)
+    parser = argparse.ArgumentParser(
+        description='Spark Job for Bike Share Data Analysis')
+    parser.add_argument('--hdfs_input_path',
+                        help='HDFS input directory', required=True, type=str)
+    parser.add_argument(
+        '--year', help='Partion Year To Process, e.g. 2024', required=True, type=str)
+    parser.add_argument(
+        '--month', help='Partion Month To Process, e.g. 11', required=True, type=str)
+    parser.add_argument(
+        '--day', help='Day of File To Process, e.g. 23', required=True, type=str)
     return parser.parse_args()
 
 
 def execute_pipeline():
-    
+
     # Parse command line Arguments
     params = parse_arguments()
-    
+
     # Initialize Spark session
     spark_ctx = pyspark.SparkContext()
     spark_session = SparkSession(spark_ctx)
 
     # Load bike share data
     bike_trips_df = spark_session.read.format('csv').options(
-            header='true',
-            delimiter=',',
-            inferschema='true',
-            nullValue="\\N"
-    ).load(params.hdfs_source_dir + '/hubway_trips/' + params.year + '/' + params.month + '/hubway_' + params.year + '-' + params.month + '-' + params.day + '.csv')
+        header='true',
+        delimiter=',',
+        inferschema='true',
+        nullValue="\\N"
+    ).load(params.hdfs_input_path + '/hubway_trips/' + params.year + '/' + params.month + '/hubway_' + params.year + '-' + params.month + '-' + params.day + '.csv')
 
-    
     # Ensure numerical columns have appropriate data types
-    bike_trips_df = bike_trips_df.withColumn("tripduration", col("tripduration").cast("int"))
-    bike_trips_df = bike_trips_df.withColumn("start station latitude", col("start station latitude").cast("float"))
-    bike_trips_df = bike_trips_df.withColumn("start station longitude", col("start station longitude").cast("float"))
-    bike_trips_df = bike_trips_df.withColumn("end station latitude", col("end station latitude").cast("float"))
-    bike_trips_df = bike_trips_df.withColumn("end station longitude", col("end station longitude").cast("float"))
+    bike_trips_df = bike_trips_df.withColumn(
+        "tripduration", col("tripduration").cast("int"))
+    bike_trips_df = bike_trips_df.withColumn(
+        "start station latitude", col("start station latitude").cast("float"))
+    bike_trips_df = bike_trips_df.withColumn(
+        "start station longitude", col("start station longitude").cast("float"))
+    bike_trips_df = bike_trips_df.withColumn(
+        "end station latitude", col("end station latitude").cast("float"))
+    bike_trips_df = bike_trips_df.withColumn(
+        "end station longitude", col("end station longitude").cast("float"))
 
-
-    
     # 1. Calculate Average Trip Duration
-    bike_trips_df = bike_trips_df.withColumn("starttime", col("starttime").cast('timestamp'))
+    bike_trips_df = bike_trips_df.withColumn(
+        "starttime", col("starttime").cast('timestamp'))
 
     bike_trips_df = bike_trips_df.withColumn('year', year(col('starttime')))
     bike_trips_df = bike_trips_df.withColumn('month', month(col('starttime')))
 
-    avg_duration_monthly = bike_trips_df.groupBy('year', 'month').agg(avg('tripduration').alias("avg_tripduration"))
+    avg_duration_monthly = bike_trips_df.groupBy('year', 'month').agg(
+        avg('tripduration').alias("avg_tripduration"))
     avg_duration_sorted = avg_duration_monthly.orderBy('year', 'month')
-    avg_duration_sorted = avg_duration_sorted.withColumn("avg_tripduration", round(avg_duration_sorted["avg_tripduration"], 1))
+    avg_duration_sorted = avg_duration_sorted.withColumn(
+        "avg_tripduration", round(avg_duration_sorted["avg_tripduration"], 1))
     avg_duration_sorted.show()
-  
 
-    # 2. Compute Average Trip Distance 
-    bike_trips_with_distance = bike_trips_df.withColumn("distance_km", sqrt((col("end station latitude") - col("start station latitude")) ** 2 + ((col("end station longitude") - col("start station longitude")) * cos(radians(col("start station latitude")))) ** 2) * 111) # Scale to kilometers
-    avg_distance_monthly = bike_trips_with_distance.groupBy("month").agg(avg("distance_km").alias("avg_trip_distance_km"))
+    # 2. Compute Average Trip Distance
+    bike_trips_with_distance = bike_trips_df.withColumn("distance_km", sqrt((col("end station latitude") - col("start station latitude")) ** 2 + (
+        # Scale to kilometers
+        (col("end station longitude") - col("start station longitude")) * cos(radians(col("start station latitude")))) ** 2) * 111)
+    avg_distance_monthly = bike_trips_with_distance.groupBy(
+        "month").agg(avg("distance_km").alias("avg_trip_distance_km"))
     avg_distance_monthly = avg_distance_monthly.orderBy("month")
     avg_distance_monthly.show()
 
-
     # 3. Usage share by gender (in percent)
-    bike_trips_df = bike_trips_df.withColumn("gender", col("gender").cast("int"))
-    gender_counts_monthly = bike_trips_df.filter(bike_trips_df["gender"] != 2) # 2 = undefined gender
-    gender_counts_monthly = gender_counts_monthly.groupBy("month", "gender").agg(count("*").alias("gender_count_month"))
-    total_gender_monthly = gender_counts_monthly.groupBy("month").agg(sum("gender_count_month").alias("total_month_gender_count"))
-    gender_usage_share = gender_counts_monthly.join(total_gender_monthly, on="month").withColumn("usage_share", round((col("gender_count_month") / col("total_month_gender_count")) * 100, 2)).select("month", "gender", "usage_share")
-    gender_usage_share = gender_usage_share.withColumn("gender", when(col("gender") == 0, "Female").otherwise("Male"))
-    gender_usage_share = gender_usage_share.orderBy(col("month"), col("gender"))
+    bike_trips_df = bike_trips_df.withColumn(
+        "gender", col("gender").cast("int"))
+    gender_counts_monthly = bike_trips_df.filter(
+        bike_trips_df["gender"] != 2)  # 2 = undefined gender
+    gender_counts_monthly = gender_counts_monthly.groupBy(
+        "month", "gender").agg(count("*").alias("gender_count_month"))
+    total_gender_monthly = gender_counts_monthly.groupBy("month").agg(
+        sum("gender_count_month").alias("total_month_gender_count"))
+    gender_usage_share = gender_counts_monthly.join(total_gender_monthly, on="month").withColumn("usage_share", round(
+        (col("gender_count_month") / col("total_month_gender_count")) * 100, 2)).select("month", "gender", "usage_share")
+    gender_usage_share = gender_usage_share.withColumn(
+        "gender", when(col("gender") == 0, "Female").otherwise("Male"))
+    gender_usage_share = gender_usage_share.orderBy(
+        col("month"), col("gender"))
     gender_usage_share.show()
 
     # Skipped the rest.
@@ -85,10 +103,12 @@ def execute_pipeline():
     gender_usage_share_pd = gender_usage_share.toPandas()
 
     with pd.ExcelWriter("/home/airflow/airflow/python/bike_share_kpi_result.xlsx", engine="openpyxl") as writer:
-        avg_duration_sorted_pd.to_excel(writer, sheet_name="Average Trip Duration", index=False)
-        avg_distance_monthly_pd.to_excel(writer, sheet_name="Average Trip Distance", index=False)
-        gender_usage_share_pd.to_excel(writer, sheet_name="Gender Usage Share", index=False)
-
+        avg_duration_sorted_pd.to_excel(
+            writer, sheet_name="Average Trip Duration", index=False)
+        avg_distance_monthly_pd.to_excel(
+            writer, sheet_name="Average Trip Distance", index=False)
+        gender_usage_share_pd.to_excel(
+            writer, sheet_name="Gender Usage Share", index=False)
 
     print("xlsx file created")
 
